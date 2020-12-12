04ed62afeb53bf6f6cd8b97e72900c7f6aa6306d
Thomas Eng <thomas.eng@shortcut.no>
2020-12-12 20:20:27 +0100

    refactor: remove old cruft

--- src/description.js
+++ src/description.js
@@ -10,7 +10,7 @@ export const LinkRel = ({ to, target, children }) => {
 export const Description = ({ onReady }) => {
   return (
     <div className='description'>
-      <h1>Velkommen!</h1>
+      <h1>Hei!</h1>
       <p>
         Dette er ett slags spill basert på stein, saks, papir,
         blandet med memory game + hurtighet.
@@ -28,16 +28,13 @@ export const Description = ({ onReady }) => {
         <img src={ScissorsPng} alt='scissors' />
         <img src={PaperPng} alt='paper' />
       </div>
-
       <p>
         Prøv å unngå ansikt innenfor rammen. Du kommer til å forvirre meg 😂
       </p>
-
       <p>
         PS: Denne tingen vil sannsynligvis ikke kjøre på mobil i det hele tatt,
         siden WebRTC, cam og GPU må være tilgjengelig for nettleser.
       </p>
-
       <p style={{
         fontWeight: 'bold'
       }}
@@ -58,6 +55,10 @@ export const Description = ({ onReady }) => {
         <LinkRel target='_blank' to='https://www.tensorflow.org/js'>TensorFlow.js</LinkRel>
         <span>+</span>
         <LinkRel target='_blank' to='https://teachablemachine.withgoogle.com/models/xf82yR2IE/'>Teachable Machine.</LinkRel>
+        <br />
+        <LinkRel target='_blank' to='https://github.com/tengla/ml-memory-game'>
+          Sjekk ut koden på GitHub
+        </LinkRel>
       </p>
     </div>
   )
